15986ea976681f3c6f925aacc578b5d3d0a08dc8
orestb <bilasorest@gmail.com>
2014-05-05 11:06:13 +0300

    RandomArray commited for checking

---
+++ RandomArray.java
@@ -0,0 +1,49 @@
+package com.lohika.orest;
+
+import java.util.Scanner;
+
+public class RandomArray {
+	static int [][] randomArray = new int[20][20];
+	
+	public static void main(String[] args) {
+		int i, j;
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Input first array's dimension, integer -");
+		int N = scan.nextInt();
+		System.out.println("Input second array's dimension, integer -");
+		int M = scan.nextInt();
+		int [][] originalArray = fillArray(N, M);
+		
+		System.out.println("Original array. Random integers from 0 to 100");
+		for (i=0; i<N; i++) {
+			for (j=0; j<M; j++) {
+				System.out.print(originalArray[i][j]+" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		System.out.println("New array. Numbers <10 replaced on 0");
+		for (i=0; i<N; i++) {
+			for (j=0; j<M; j++) {
+				if (originalArray[i][j]<10) {
+					System.out.print(0+" ");
+				}
+				else {
+					System.out.print(originalArray[i][j]+" ");
+				}
+				}
+			System.out.println();
+			}
+			
+			}
+public static int[][] fillArray(int N, int M) {
+	int i, j;
+	for (i=0; i<N; i++) {
+		for (j=0; j<M; j++) {
+			randomArray [i][j] = (int) (100*Math.random());
+		}
+	}
+	return randomArray;
+}
+}
